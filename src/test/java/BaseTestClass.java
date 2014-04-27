dcf89f3b6f37928019c946846938d9a5fa1d628f
markshao <37647536@qq.com>
2014-04-27 14:14:22 +0800

    add the base test class

---
+++ src/test/java/BaseTestClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mark
+ * Date: 14-4-27
+ * Time: 下午2:13
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class BaseTestClass {
+}
